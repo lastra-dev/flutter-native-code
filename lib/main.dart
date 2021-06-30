36a199b7f0048d64b36bca40dc71ecfb42f6c915
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2021-06-30 13:09:00 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static const platform = const MethodChannel('samples.flutter.dev/battery');
+
+  //Get battery level
+  String _batteryLevel = 'Unkown battery level.';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result% .';
+    } on PlatformException catch (e) {
+      batteryLevel = 'Failed to get battery level: ${e.message}';
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Battery Level',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Battery Level'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                child: Text('Get Battery Level'),
+                onPressed: _getBatteryLevel,
+              ),
+              Text(_batteryLevel),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
